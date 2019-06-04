16ad4bb0a9ba8fef813bceab655c2681c606601e
Alessandro Losi <pipipe550@hotmail.com>
2019-06-04 16:18:39 +0200

    First steps

--- src/epiclauncher.py
+++ src/epiclauncher.py
@@ -4,33 +4,25 @@ import keypirinha as kp
 import keypirinha_util as kpu
 import keypirinha_net as kpnet
 
+import json
+import os
+import collections
+
+GAME_LAUNCH_URL = "com.epicgames.launcher://apps/{}?action=launch&silent=true"
+ALL_USERS_PATH = "c:\\Users\\All Users\\Epic\\"
+
+InstalledApp = collections.namedtuple('InstalledApp', ['InstallLocation', 'AppName', 'AppID', 'AppVersion'])
+
+
 class EpicLauncher(kp.Plugin):
     """
-    One-line description of your plugin.
+    Add installed Epic Games Launcher games to the Keypirinha Catalog.
 
-    This block is a longer and more detailed description of your plugin that may
-    span on several lines, albeit not being required by the application.
-
-    You may have several plugins defined in this module. It can be useful to
-    logically separate the features of your package. All your plugin classes
-    will be instantiated by Keypirinha as long as they are derived directly or
-    indirectly from :py:class:`keypirinha.Plugin` (aliased ``kp.Plugin`` here).
-
-    In case you want to have a base class for your plugins, you must prefix its
-    name with an underscore (``_``) to indicate Keypirinha it is not meant to be
-    instantiated directly.
-
-    In rare cases, you may need an even more powerful way of telling Keypirinha
-    what classes to instantiate: the ``__keypirinha_plugins__`` global variable
-    may be declared in this module. It can be either an iterable of class
-    objects derived from :py:class:`keypirinha.Plugin`; or, even more dynamic,
-    it can be a callable that returns an iterable of class objects. Check out
-    the ``StressTest`` example from the SDK for an example.
-
-    Up to 100 plugins are supported per module.
-
-    More detailed documentation at: http://keypirinha.com/api/plugin.html
+    Version: 0.1
     """
+
+    CATEGORY = kp.ItemCategory.USER_BASE + 1
+
     def __init__(self):
         super().__init__()
 
@@ -38,13 +30,27 @@ class EpicLauncher(kp.Plugin):
         pass
 
     def on_catalog(self):
-        pass
+        applist = self.get_applist()
+
+        items = [
+            self.create_item(
+                category=self.CATEGORY,
+                label=app.AppName,
+                target=app.AppName,
+                short_desc="Launch game",
+                args_hint=kp.ItemArgsHint.ACCEPTED,
+                hit_hint=kp.ItemHitHint.KEEPALL)
+            for app in applist]
+
+        self.set_catalog(items)
 
     def on_suggest(self, user_input, items_chain):
         pass
 
     def on_execute(self, item, action):
-        pass
+        appname = item.target()
+        target = GAME_LAUNCH_URL.format(appname)
+        kpu.shell_execute(target)
 
     def on_activated(self):
         pass
@@ -54,3 +60,21 @@ class EpicLauncher(kp.Plugin):
 
     def on_events(self, flags):
         pass
+
+    def get_applist(self):
+        results = []
+
+        install_path = os.path.join(ALL_USERS_PATH, "UnrealEngineLauncher", "LauncherInstalled.dat")
+        if not os.path.exists(install_path) or not os.path.isfile(install_path):
+            self.warn("No launcher data found")
+            return results
+
+        with open(install_path, "r") as data_file:
+            installed = json.load(data_file)
+
+        for entry in installed.InstallationList:
+            app = InstalledApp(entry.InstallLocation, entry.AppName, entry.AppID, entry.AppVersion)
+
+            results.append(app)
+
+        return results
